26f234d8fff0632eddab709f26a37c4522599901
Narinder Lakhani <lakhani9@yahoo.com>
2026-05-30 05:56:51 -0400

    fix config to use DATABASE_URL from environment

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-change-in-production")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///fieldforce.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL", "sqlite:///fieldforce.db"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ENGINE_OPTIONS = {
         "pool_pre_ping": True,
